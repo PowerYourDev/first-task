f552702e2db4bb618351c4d81473135660c0b30d
saiteja <saitejachicky@gmail.com>
2023-03-25 20:49:26 +0530

    check-box

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const[checked,setchecked]=useState(false)
+  const checkBox =()=>{
+  setchecked(!checked)
+  consoleInfo(checked)
+     
+  }
+  const consoleInfo=(checked)=>{
+    !checked ? console.log("checked") : console.log("not checked")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='body'>
+
+    
+    <input  onChange={checkBox} checked={checked} type="checkbox"/>
+   
+
+   <div className='body-info'>
+       <h1>Verified by ConQt</h1>
+       <h2>Check when product is verified by ConQt</h2>
     </div>
-  );
+     
+   
+   </div>
+  )
 }
 
-export default App;
+export default App
